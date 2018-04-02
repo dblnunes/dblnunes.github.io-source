0910718b0a1b5bd4f782af7f045acea7b6f98437
Daniel Nunes <daninunes@192.168.1.106>
2018-04-02 22:04:05 +0100

    commit message

---
+++ content/articles/test-md-post.md
@@ -0,0 +1,21 @@
+Title: Markdown post test
+Slug: mardown-post
+Date: 2017-03-30 20:00
+Category: posts
+Tags: python, mardown, firsts
+Author: Daniel Nunes
+Summary: This is post is written using markdown.
+
+# This post is written in Markdown
+
+## Ordered lists
+
+1. first item
+2. second item
+
+
+## Code blocks
+
+```python
+print("this is python code)
+```
---
+++ pelicanconf.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Daniel Nunes'
+SITENAME = 'Daniel Nunes'
+SITESUBTITLE = u'Learning Data Science and Machine Learning'
+SITEURL = 'https://dblnunes.github.io'
+PATH = 'content'
+TIMEZONE = 'Europe/Paris'
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+# Set the article URL
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+
+
+# THEME SETTINGS
+THEME = './theme/'
+
+ABOUT_PAGE = '/pages/about.html'
+TWITTER_USERNAME = 'dbl__nunes'
+GITHUB_USERNAME = 'dblnunes'
+
+
+AUTHOR_BLOG = 'http://dblnunes.github.io'
+
+SHOW_ARCHIVES = True
+SHOW_FEED = False  # Need to address large feeds
+
+ENABLE_MATHJAX = True
+
+STATIC_PATHS = ['images', 'figures', 'videos', 'downloads', 'favicon.png']
+
+# Footer info
+
+
+LICENSE = "MIT"
