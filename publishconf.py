7455289fdd8fa1a7907d4afb28e1a66938be275d
Daniel Nunes <daninunes@192.168.1.106>
2018-04-02 23:55:49 +0100

    fix relative url

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'http://dblnunes.github.io/'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 SHOW_FEED = True
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
